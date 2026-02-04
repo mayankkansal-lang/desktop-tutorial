64be5ce9431a23b5b9abb28e1efc75619d9694c4
krishankantks <krishankant@truinc.com>
2026-02-04 06:37:32 +0000

    asdfasdfa

--- Learning/chapter-2.js
+++ Learning/chapter-2.js
@@ -1,1 +1,7 @@
 console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
+console.log("asdfasdfasdf")
