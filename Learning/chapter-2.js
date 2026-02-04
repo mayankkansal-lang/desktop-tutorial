a05b2a897766e046f2c9b01924a69f689be2e49d
mayankkansal <mayank.kansal@truinc.com>
2026-02-04 12:19:14 +0530

    asdf

--- Learning/chapter-2.js
+++ Learning/chapter-2.js
@@ -1,7 +1,8 @@
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
-console.log("asdfasdfasdf")
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("asdfasdfasdf");
+console.log("123123123123123");
